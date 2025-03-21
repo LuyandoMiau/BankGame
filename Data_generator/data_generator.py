6633ff44f72ff07b6a6a349ab1f62e57bdf5de90
LuyandoMiau <luyando333@gmail.com>
2025-03-21 12:40:40 +0100

    first trial data_generator

---
+++ Data_generator/data_generator.py
@@ -0,0 +1,33 @@
+### Packages to be imported
+
+# Data Management and Processing
+import pandas as pd
+import numpy as np
+import scipy
+
+# Machine Learning and Statistics
+import sklearn
+import statsmodels.api as sm
+import tensorflow as tf
+#import tensorflowjs as tfjs
+
+# Data visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Web development (but I will be using .css and .html)
+from flask import Flask 
+from fastapi import FastAPI 
+import uvicorn
+
+# Data sources for Finance
+import yfinance as yf
+from alpha_vantage.timeseries import TimeSeries
+import requests
+
+
+
+
+
+
