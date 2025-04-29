abd0340cdf7d371afe1478051138c9a67de7f817
LuyandoMiau <luyando333@gmail.com>
2025-04-29 21:32:11 +0200

    First SQL queries setup

---
+++ Data_generator/SQL_queries/SQL_queries_Data.py
@@ -0,0 +1,22 @@
+""""Let"s call the necessary variables from DataEditingVariableSetup.py"""
+# Packages
+import sys
+import os
+
+# Add the folder containing data_generator.py to the Python path
+sys.path.append(os.path.abspath('/Users/bonjour/Documents/AI/Projects/GitHub/BankGame/Data_generator'))
+from data_generator import saving_path_statistics
+
+
+"""Let"s use the sqlite3 package to make SQL queries on the generated data"""
+
+# Packages
+import sqlite3
+import pandas as pd
+
+# Get our data frame and save it to a SQL database
+df_SQL = pd.read_csv(saving_path_statistics) 
+conn = sqlite3.connect('customers_data.db')                    
+df_SQL.to_sql('customers_data', conn, if_exists='replace', index=False) 
+conn.close()  
+
